38a47d83ce0a393d8ef32aae7740d51ad378373e
Marcelo Fernandez <marcelo.fernandez@global66.com>
2024-05-26 00:17:15 -0400

    first commit

---
+++ src/app/signin/signin.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from "@angular/common";
+
+@Component({
+  selector: 'app-signin',
+  standalone: true,
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './signin.component.html',
+  styleUrl: './signin.component.css'
+})
+export class SigninComponent {
+
+}
